93d8e1aba726155c08480f878a3157ec81781e64
sam <sampreethm@proton.me>
2023-06-04 12:46:17 +0530

    html editor 🥺

---
+++ h/app.js
@@ -0,0 +1,45 @@
+
+const first = document.querySelector(".first");
+const iframe = document.querySelector("iframe");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  run();
+});
+
+first.addEventListener("paste", function(e) {
+        e.preventDefault();
+        var text = e.clipboardData.getData("text/plain");
+        document.execCommand("insertText", false, text);
+    });
+
+
+document.addEventListener("keydown", function(event) {
+    if (event.ctrlKey && event.key === "Enter") {
+        run();
+    }
+});
+
+function run() {
+    var html = first.textContent;
+    iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
+}
+
+
+const preElement = document.querySelector(".first pre");
+
+
+const htmlCode = `<!DOCTYPE html>
+<html>
+    <head>
+        <style></style>
+    </head>
+    <body>
+    hello, world!
+    <script></script>
+    </body>
+</html>`;
+
+
+preElement.textContent = htmlCode;
+
